baa221b4b812bc39547fef8a24044d764d7bb78c
Soni290190 <sonigustiana1990@gmail.com>
2024-10-01 14:29:56 +0700

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -2,14 +2,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBgBwfUpy6pgzL5GNlntzxZ57mmqSkA_XQ",
   authDomain: "webanalisisdampaklalulintas.firebaseapp.com",
-  databaseURL: "https://webanalisisdampaklalulintas-default-rtdb.firebaseio.com",
   projectId: "webanalisisdampaklalulintas",
   storageBucket: "webanalisisdampaklalulintas.appspot.com",
   messagingSenderId: "435784394834",
   appId: "1:435784394834:web:698c257010dee577cf178b",
-  measurementId: "G-PE2FWCP87Y"
 };
 
-// Initialize Firebase
+// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
