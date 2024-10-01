423e9c705321525a82157e39a27bd3077b3c460e
Soni290190 <sonigustiana1990@gmail.com>
2024-10-01 17:55:43 +0700

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,12 +1,23 @@
-// Your web app's Firebase configuration
+// Import Firebase SDKs
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, addDoc, onSnapshot, doc } from "firebase/firestore"; 
+
+// Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBgBwfUpy6pgzL5GNlntzxZ57mmqSkA_XQ",
   authDomain: "webanalisisdampaklalulintas.firebaseapp.com",
+  databaseURL: "https://webanalisisdampaklalulintas-default-rtdb.firebaseio.com",
   projectId: "webanalisisdampaklalulintas",
   storageBucket: "webanalisisdampaklalulintas.appspot.com",
   messagingSenderId: "435784394834",
   appId: "1:435784394834:web:698c257010dee577cf178b",
+  measurementId: "G-PE2FWCP87Y"
 };
 
-// Inisialisasi Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore(app);
